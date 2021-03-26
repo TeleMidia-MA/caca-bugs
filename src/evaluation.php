b1f9ed745745585b7bca464b3b0f58b976493716
Dillsy <elydillselindoso@gmail.com>
2021-03-26 16:47:55 -0300

    alterando nome do diretorio para src

---
+++ src/evaluation.php
@@ -0,0 +1,24 @@
+<?php
+   include("config.php");
+   include('session.php');
+   
+   $user_check = $_SESSION['login_user'];
+   //session_start();
+   $error = "";
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      echo $_SERVER['login_user'];
+      $myvalue = mysqli_real_escape_string($db,$_POST['value']);
+      $myuser = $_SESSION['id_user'];//mysqli_real_escape_string($db,$user_check);
+      $myquestion = mysqli_real_escape_string($db,$_POST['code']);
+      $myhasLiked = mysqli_real_escape_string($db,$_POST['like']);
+
+      $sql = "INSERT INTO evaluation(nota, question, user, hasLiked) values ('$myvalue', '$myquestion', '$myuser', '$myhasLiked')";
+          
+    if (mysqli_query($db, $sql)) {
+        header('location: index.php');
+    } else {
+        echo "Error updating record: " . mysqli_error($db);
+      }
+   }
+?>
