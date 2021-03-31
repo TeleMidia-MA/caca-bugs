639f874f07899171d3d80eb97194e28c7016dfa4
D'jefferson Smith Santos Maranhão <djefferson.maranhao@gmail.com>
2021-03-31 14:10:43 -0300

    alteração no arquivo session

--- src/session.php
+++ src/session.php
@@ -2,17 +2,19 @@
    include('config.php');
    session_start();
    
-   $user_check = $_SESSION['login_user'];
-   
-   $ses_sql = mysqli_query($db,"select * from user where email = '$user_check' ");
-   if (!$ses_sql) {
-	    printf("Error: %s\n", mysqli_error($db));
-	    exit();
-	}
-   
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-   
-   $login_session = array("user" => $row['email'], "id"=>$row['id']);
+   if(isset($_SESSION['login_user'])){
+	   $user_check = $_SESSION['login_user'];
+	   
+	   $ses_sql = mysqli_query($db,"select * from user where email = '$user_check' ");
+	   if (!$ses_sql) {
+			printf("Error: %s\n", mysqli_error($db));
+			exit();
+		}
+	   
+	   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+	   
+	   $login_session = array("user" => $row['email'], "id"=>$row['id']);
+   }
    
    if(!isset($_SESSION['login_user'])){
       header("location:login.php");
