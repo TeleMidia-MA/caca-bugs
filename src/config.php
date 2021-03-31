ca6badcb4be2e4f2a5ad2c6c852e60d219d2ab2c
D'jefferson Smith Santos Maranhão <djefferson.maranhao@gmail.com>
2021-03-31 15:28:58 -0300

    Removendo erros e warnings do console.

--- src/config.php
+++ src/config.php
@@ -1,7 +1,7 @@
 <?php
-   //define('DB_SERVER', 'db-caca-bugs');
-   //define('DB_USERNAME', 'root');
-   //define('DB_PASSWORD', 'opECcbQ^tF!EA');
-   //define('DB_DATABASE', 'caca_bugs');
-   $db = mysqli_connect(getenv("DB_SERVER"),getenv("DB_USERNAME"),getenv("DB_PASSWORD"),getenv("DB_DATABASE")) or die(mysql_error());
+   define('DB_SERVER', 'db-caca-bugs');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'opECcbQ^tF!EA');
+   define('DB_DATABASE', 'caca_bugs');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die(mysql_error());
 ?>
